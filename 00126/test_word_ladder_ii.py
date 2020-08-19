a7487ed8b18a30c22886692e497c6a391a9d8ea4
SinCatGit <sincat@126.com>
2020-08-19 09:52:34 +0800

    fix: word-ladder-ii

--- 00126/test_word_ladder_ii.py
+++ 00126/test_word_ladder_ii.py
@@ -5,10 +5,11 @@ from word_ladder_ii import Solution
 class TestSolution(unittest.TestCase):
     def test_isValid_Solution(self):
         sol = Solution()
-        self.assertEqual(
-            [["hit", "hot", "dot", "dog", "cog"], ["hit", "hot", "lot", "log", "cog"]],
-            sol.findLadders(beginWord="hit", endWord="cog", wordList=["hot", "dot", "dog", "lot", "log", "cog"])
-        )
+        source = [["hit", "hot", "dot", "dog", "cog"], ["hit", "hot", "lot", "log", "cog"]]
+        expects = {''.join(item) for item in source}
+        ret = sol.findLadders(beginWord="hit", endWord="cog", wordList=["hot", "dot", "dog", "lot", "log", "cog"])
+        results = {''.join(item) for item in ret}
+        self.assertEqual(expects, results)
         self.assertEqual([], sol.findLadders(beginWord="hit", endWord="cog", wordList=["hot","dot","dog","lot","log"]))
 
 
